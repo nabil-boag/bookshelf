3e521321c3bc9b352fda5a184a36f93342029c95
Nabil Boag <nabilboag@gmail.com>
2016-02-19 00:06:29 +0000

    Updated demo

--- app/index.php
+++ app/index.php
@@ -4,11 +4,10 @@
 <?php 
 
 $connection_envs = array(
-    'driver' => 'pdo_mysql',
-    'dbname' => isset($_SERVER['MYSQL_DATABASE']) ? $_SERVER['MYSQL_DATABASE'] : null,
-    'user' => isset($_SERVER['MYSQL_USERNAME']) ? $_SERVER['MYSQL_USERNAME'] : null,
-    'password' => isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : null ,
-    'host' => isset($_SERVER['MYSQL_HOST']) ? $_SERVER['MYSQL_HOST'] : null,
+    'dbname' => isset($_SERVER['MYSQL_DATABASE']) ? $_SERVER['MYSQL_DATABASE'] : 'demoapp',
+    'dbuser' => isset($_SERVER['MYSQL_USERNAME']) ? $_SERVER['MYSQL_USERNAME'] : 'root',
+    'dbpass' => isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'mysupersecretrootpassword',
+    'dbhost' => isset($_SERVER['MYSQL_HOST']) ? $_SERVER['MYSQL_HOST'] : 'mysql',
 );
 
 var_dump($connection_envs);
@@ -16,10 +15,10 @@ var_dump($connection_envs);
 
 <h2>Mysql Local</h2>
 <?php 
-$dbname = 'demoapp';
-$dbuser = 'root';
-$dbpass = 'mysupersecretrootpassword';
-$dbhost = 'mysql';
+$dbname = $connection_envs['dbname'];
+$dbuser = $connection_envs['dbuser'];
+$dbpass = $connection_envs['dbpass'];
+$dbhost = $connection_envs['dbhost'];
 $connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
 mysql_select_db($dbname) or die("Could not open the db '$dbname'");
 $test_query = "SHOW TABLES FROM $dbname";
