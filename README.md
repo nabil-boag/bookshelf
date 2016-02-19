bd5f53b8a0d83108b5b7256ed13830770af3ddc1
nabil-boag <nabilgod@gmail.com>
2016-02-19 15:04:40 +0000

    Tweaks 2

--- README.md
+++ README.md
@@ -7,21 +7,25 @@
 * [Docker](https://docs.docker.com/engine/installation/)
 * [Docker compose](https://docs.docker.com/compose/install/)
 
-### Deploying Production
-
-* [EB CLI](http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/eb-cli3-install.html)
-
-### Docker environment setup
+#### Docker environment setup
 
 ```
+export MYSQL_ROOT_PASSWORD=mysupersecretrootpassword MYSQL_HOST=mysql MYSQL_DATABASE=demoapp MYSQL_USERNAME=root MYSQL_PASSWORD=mysupersecretrootpassword
 cp docker/development/docker-compose.yml.development docker-compose.yml
 docker-compose build
 docker-compose up -d
 ```
 
-### Prepare database
+#### Prepare database
 
 ```
-docker exec -ti bookshelf_mysql_1 mysql -pmysupersecretrootpassword -e 'create database demoapp'
-docker exec -ti  bookshelf_fpm_1 bash -c 'cd bookshelf; vendor/bin/doctrine orm:schema-tool:create'
+docker exec -ti dockerebsexample_mysql_1 mysql -pmysupersecretrootpassword -e 'create database demoapp'
 ```
+
+### Deploying Production
+
+* [EB CLI](http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/eb-cli3-install.html)
+
+#### Preparing
+
+@TODO
--- app/index.php
+++ app/index.php
@@ -1,28 +1,28 @@
 <h1>Example PHP App</h1>
 
 <h2>Mysql Connection Properites</h2>
-<?php 
-var_dump(getenv('PARAM1'));
-
-
+<?php
+var_dump($_SERVER['PARAM1']);
+var_dump($_SERVER['PARAM2']);
 $connection_envs = array(
-    'dbname' => isset($_SERVER['MYSQL_DATABASE']) ? $_SERVER['MYSQL_DATABASE'] : 'demoapp',
-    'dbuser' => isset($_SERVER['MYSQL_USERNAME']) ? $_SERVER['MYSQL_USERNAME'] : 'root',
-    'dbpass' => isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'mysupersecretrootpassword',
-    'dbhost' => isset($_SERVER['MYSQL_HOST']) ? $_SERVER['MYSQL_HOST'] : 'mysql',
+    'dbhost' => isset($_SERVER['MYSQL_HOST']) ? $_SERVER['MYSQL_HOST'] : null,
+    'dbname' => isset($_SERVER['MYSQL_DATABASE']) ? $_SERVER['MYSQL_DATABASE'] : null,
+    'dbuser' => isset($_SERVER['MYSQL_USERNAME']) ? $_SERVER['MYSQL_USERNAME'] : null,
+    'dbpass' => isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : null,
 );
 
-// var_dump($connection_envs);
+var_dump($connection_envs);
 ?>
 
-<h2>Mysql Local</h2>
-<?php 
+<h2>Mysql Connected?</h2>
+<?php
 $dbname = $connection_envs['dbname'];
 $dbuser = $connection_envs['dbuser'];
 $dbpass = $connection_envs['dbpass'];
 $dbhost = $connection_envs['dbhost'];
 $connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
 mysql_select_db($dbname) or die("Could not open the db '$dbname'");
+echo "Connected! ";
 $test_query = "SHOW TABLES FROM $dbname";
 $result = mysql_query($test_query);
 $tblCnt = 0;
@@ -31,7 +31,7 @@ while($tbl = mysql_fetch_array($result)) {
   #echo $tbl[0]."<br />\n";
 }
 if (!$tblCnt) {
-  echo "There are no tables<br />\n";
+  echo "There are no tables though<br />\n";
 } else {
   echo "There are $tblCnt tables<br />\n";
 }
